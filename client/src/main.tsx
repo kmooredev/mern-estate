77d8ed4674abecce1862c5e9cb08fc526954f4be
Korey Moore <kmooredev@gmail.com>
2023-10-23 17:04:53 -0500

    added redux-persist

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,9 +4,13 @@ import App from './App';
 import './index.css';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
