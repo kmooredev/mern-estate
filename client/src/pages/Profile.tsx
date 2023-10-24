88874a28f48afd101ea78a6f4ea80494b1474066
Korey Moore <kmooredev@gmail.com>
2023-10-24 16:39:42 -0500

    complete profile page UI

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,5 +1,45 @@
+import React from 'react';
+import { useAppSelector } from '../redux/hooks';
+
 const Profile = () => {
-  return <div>This is the Profile component.</div>;
+  const { currentUser } = useAppSelector((state) => state.user);
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          className="rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2"
+          src={currentUser.avatar}
+          alt="profile photo"
+        />
+        <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounded-lg-2"
+          id="username"
+        />
+        <input
+          type="text"
+          placeholder="email"
+          className="border p-3 rounded-lg-2"
+          id="email"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="border p-3 rounded-lg-2"
+          id="password"
+        />
+        <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5 ">
+        <span className="text-red-700 cursor-pointer ">Delete Account</span>
+        <span className="text-red-700 cursor-pointer ">Sign Out</span>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
