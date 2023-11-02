d911445dfa6b057488b59c8373fe2cb2bc414926
Korey Moore <kmooredev@gmail.com>
2023-11-01 19:39:10 -0500

    added Create Listing UI

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,5 +1,6 @@
 import React, { ChangeEvent } from 'react';
 import { useRef, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useAppSelector, useAppDispatch } from '../redux/hooks';
 import { app } from '../firebase';
 import {
@@ -196,6 +197,11 @@ const Profile = () => {
           className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
           {isLoading ? 'Loading...' : 'Update'}
         </button>
+        <Link
+          to={'/create-listing'}
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95">
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5 ">
         <span
