a75aa8cb4f5acecb7e4075c54db5b3eee7592be8
Korey Moore <kmooredev@gmail.com>
2023-11-13 12:40:37 -0600

    Add updateListing function and route

--- api/controllers/listing.controller.ts
+++ api/controllers/listing.controller.ts
@@ -36,3 +36,23 @@ export const deleteListing = async (req: any, res: any, next: NextFunction) => {
     next(error);
   }
 };
+
+export const updateListing = async (req: any, res: any, next: NextFunction) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, 'Listing not found'));
+  }
+
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, 'You can only update your own listings!'));
+  }
+
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    return next(error);
+  }
+};
--- api/routes/listing.route.ts
+++ api/routes/listing.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createListing, deleteListing } from '../controllers/listing.controller.ts';
+import { createListing, deleteListing, updateListing } from '../controllers/listing.controller.ts';
 import { verifyToken } from '../utils/verifyUser.ts';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, updateListing);
 
 export default router;
