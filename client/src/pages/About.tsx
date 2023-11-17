acd0f3cc46c862b703342a6c6eba4c7413b11c8b
Korey Moore <kmooredev@gmail.com>
2023-11-16 18:25:03 -0600

    Fix HTML and update About page content.

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,5 +1,36 @@
+import React from 'react';
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Moore Estate</h1>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate voluptate, neque vel
+        unde autem quos molestiae quibusdam illo dolorem exercitationem molestias assumenda nihil
+        qui dicta praesentium cumque velit earum. Laboriosam? Hic a commodi beatae facere cum quidem
+        expedita tempora quam et voluptate alias, necessitatibus velit est, nesciunt consectetur
+        repudiandae possimus veritatis fuga mollitia voluptas aliquam laudantium? Similique quo
+        eaque doloremque. Corporis dolorem consectetur reprehenderit veritatis eos soluta voluptate
+        nisi tempora, optio necessitatibus nostrum accusamus! Sit autem nulla rem aperiam, non
+        dolorem velit laudantium nam corporis accusantium hic voluptatibus a. Quasi.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate voluptate, neque vel
+        unde autem quos molestiae quibusdam illo dolorem exercitationem molestias assumenda nihil
+        qui dicta praesentium cumque velit earum.
+      </p>
+      <p className='mb-4 text-slate-700'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate voluptate, neque vel
+        unde autem quos molestiae quibusdam illo dolorem exercitationem molestias assumenda nihil
+        qui dicta praesentium cumque velit earum. Laboriosam? Hic a commodi beatae facere cum quidem
+        expedita tempora quam et voluptate alias, necessitatibus velit est, nesciunt consectetur
+        repudiandae possimus veritatis fuga mollitia voluptas aliquam laudantium? Similique quo
+        eaque doloremque. Corporis dolorem consectetur reprehenderit veritatis eos soluta voluptate
+        nisi tempora, optio necessitatibus nostrum accusamus! Sit autem nulla rem aperiam, non
+        dolorem velit laudantium nam corporis accusantium hic voluptatibus a. Quasi.
+      </p>
+    </div>
+  );
 };
 
 export default About;
