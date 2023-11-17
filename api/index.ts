f7c750fa4a3f00c5d7b7d420bfb0e730d0187a41
Korey Moore <kmooredev@gmail.com>
2023-11-17 13:49:21 -0600

    add pre-installation

--- api/index.ts
+++ api/index.ts
@@ -5,8 +5,11 @@ import userRouter from './routes/user.route.ts';
 import authRouter from './routes/auth.route.ts';
 import listingRouter from './routes/listing.route.ts';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -29,14 +32,18 @@ app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
 
-app.use(
-  (err: Error | any, _req: Request, res: Response, _nextTick: NextFunction) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
-    return res.status(statusCode).json({
-      success: false,
-      statusCode,
-      message,
-    });
-  }
-);
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (_req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+app.use((err: Error | any, _req: Request, res: Response, _nextTick: NextFunction) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
